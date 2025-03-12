c6b6438a9646aad0313be7bc9d8b970ec9a6d340
胡含 <2binfront@gmail.com>
2025-03-12 17:59:38 +0800

    tmp thread compare

--- C++/threads/thread_compare.c
+++ C++/threads/thread_compare.c
@@ -5,9 +5,26 @@
 
 static thread_t threads[NTHREADS];
 
-static void thread_func(int n){
-		thread_exit(0);
-	//	return n;//never arrive here
+
+// 一个简单的循环，不断递增一个计数器，并在该计数器的值可被 10000000 整除时打印一个句点（“.”）
+static void simple_loop(){
+    int i=0;
+    while(1){
+        i++;
+        if(i%10000000==0){
+            printf(".");
+        }
+    }
+}
+
+// 反复等待用户输入一行文本，然后打印“谢谢您的输入”
+static void user_input(){
+    char buf[100];
+    while(1){
+        printf("Please input something: ");
+        fgets(buf, 100, stdin);
+        printf("Thank you for your input.\n");
+    }
 }
 
 int main(int argc, char **argv){
@@ -21,9 +38,9 @@ int main(int argc, char **argv){
 
 		clock_t create_start = clock();
 
-		for (i=0; i<NTHREADS; i++){
-				thread_create(&threads[i], &thread_func, i);
-		}
+        // 创建两个线程，运行上述两个函数
+    	thread_create(&threads[0], &simple_loop,NULL);
+	    thread_create(&threads[1], &user_input, NULL);
 
 		clock_t create_end = clock();
 		create_time = ((double) (create_end - create_start)) / CLOCKS_PER_SEC;
